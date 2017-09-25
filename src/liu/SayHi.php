1ed4f1b657fb334ef3889a3035c3b96e38253cbe
刘恒胜 <liuhengsheng@xunlei.com>
2017-09-25 13:58:40 +0800

    just commit a test script file

---
+++ src/liu/SayHi.php
@@ -0,0 +1,17 @@
+<?php
+namespace composertest;
+
+class SayHi{
+    private $name;
+    private $from;
+
+
+    public function __construct() {
+        $this->name = "liuhengsheng";
+        $this->from = "hubei";
+    }
+
+    public function sayhi() {
+        echo sprintf("my name is %s, I'm from %s, welcome to beijing\n", $this->name, $this->from);
+    }
+}
